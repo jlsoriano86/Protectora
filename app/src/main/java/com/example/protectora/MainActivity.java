5ce36fc55d7d823624602e6eaaf07061078d85f5
Jose Luis Soriano <35837723+jlsoriano86@users.noreply.github.com>
2019-02-18 17:18:28 +0100

    Creación de la pantalla Principal

--- app/src/main/java/com/example/protectora/MainActivity.java
+++ app/src/main/java/com/example/protectora/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.protectora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        entrar = (Button)findViewById(R.id.btnEntrar);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entrar = new Intent(MainActivity.this, Principal.class);
+                startActivity(entrar);
+            }
+        });
     }
 }
