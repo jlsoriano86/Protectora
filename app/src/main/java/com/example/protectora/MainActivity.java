dc36ec492e47cb8afc2860f4758d1cb7f51c5320
Jose Luis Soriano <35837723+jlsoriano86@users.noreply.github.com>
2019-02-18 18:03:18 +0100

    cambios en login

--- app/src/main/java/com/example/protectora/MainActivity.java
+++ app/src/main/java/com/example/protectora/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.protectora;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtNombreUsuario, edittxtContrasenaUsuario;
     Button entrar;
 
     @Override
@@ -15,13 +18,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         entrar = (Button)findViewById(R.id.btnEntrar);
+        txtNombreUsuario = (EditText) findViewById(R.id.txtNombreUsuario);
+        edittxtContrasenaUsuario = (EditText) findViewById(R.id.txtContrasenaUsuario);
 
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent entrar = new Intent(MainActivity.this, Principal.class);
-                startActivity(entrar);
+            public void onClick(View view) {
+                usuarioLogin();
             }
         });
+    }
+
+    private void usuarioLogin() {
+        final String usuarioNombre = txtNombreUsuario.getText().toString();
+        final String usuarioContrasena = edittxtContrasenaUsuario.getText().toString();
+
+        if (TextUtils.isEmpty(usuarioNombre)) {
+            txtNombreUsuario.setError("Por favor, introduce tu nombre de usuario");
+            txtNombreUsuario.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(usuarioContrasena)) {
+            edittxtContrasenaUsuario.setError("Por favor, introduce una contraseña");
+            edittxtContrasenaUsuario.requestFocus();
+            return;
+
+
+            //PRUEBA DE AQUÍ AL FINAL
+            
+
+        }
     }
 }
