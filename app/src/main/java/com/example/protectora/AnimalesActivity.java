f026f2ea589c44b41c56be37977c4ad861516055
jlsoriano86 <josesorna86@gmail.com>
2019-05-13 21:18:05 +0200

    Creación de la baja y modificación de animales

--- app/src/main/java/com/example/protectora/AnimalesActivity.java
+++ app/src/main/java/com/example/protectora/AnimalesActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 
 public class AnimalesActivity extends AppCompatActivity {
     Button altaAnimales;
+    Button consultaAnimales;
+    Button bajaAnimales;
+    Button modificarAnimales;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,33 @@ public class AnimalesActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent altaAnimales = new Intent(AnimalesActivity.this, AltaAnimalesActivity.class);
                 startActivity(altaAnimales);
+            }
+        });
+
+        consultaAnimales = findViewById(R.id.btnConsultarAnimales);
+       consultaAnimales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent consultaAnimales = new Intent(AnimalesActivity.this, ConsultaAnimalesActivity.class);
+                startActivity(consultaAnimales);
+            }
+        });
+
+        bajaAnimales = findViewById(R.id.btnBajaAnimales);
+        bajaAnimales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bajaAnimales = new Intent(AnimalesActivity.this, BajaAnimalesActivity.class);
+                startActivity(bajaAnimales);
+            }
+        });
+
+        modificarAnimales = findViewById(R.id.btnModificarAnimales);
+        modificarAnimales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent modificarAnimales = new Intent(AnimalesActivity.this, ModificaAnimalesActivity.class);
+                startActivity(modificarAnimales);
             }
         });
     }
---
+++ app/src/main/java/com/example/protectora/ConsultaAnimalesActivity.java
@@ -0,0 +1,13 @@
+package com.example.protectora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultaAnimalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_animales);
+    }
+}
