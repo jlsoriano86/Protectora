ce7cb317c270febcecf3a13dab0ccfa567cf2f6f
jlsoriano86 <josesorna86@gmail.com>
2019-06-02 17:23:24 +0200

    login finalizado

--- app/src/main/java/com/example/protectora/ConsultaAnimalesActivity.java
+++ app/src/main/java/com/example/protectora/ConsultaAnimalesActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,10 +13,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -23,25 +28,50 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 public class ConsultaAnimalesActivity extends AppCompatActivity {
     Spinner spAnimal;
+    String URL="http://protectora-animales.ddns.net/phpMyAdmin/api/animals/getAnimals.php";
+    ArrayList<String> lstAnimales;
+
     ImageView imgImagen;
     TextView txtId, txtNacimiento, txtTipo, txtEstado;
     RequestQueue requestQueue;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta_animales);
-
+        lstAnimales=new ArrayList<>();
         spAnimal = (Spinner) findViewById(R.id.spAnimal);
+        buscarAnimal(URL);
+
+
+
         imgImagen = (ImageView) findViewById(R.id.imgImagen);
         txtId = (TextView) findViewById(R.id.txtId);
         txtNacimiento = (TextView) findViewById(R.id.txtNacimiento);
         txtTipo = (TextView) findViewById(R.id.txtTipo);
         txtEstado = (TextView) findViewById(R.id.txtEstado);
+        spAnimal.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String animal= spAnimal.getItemAtPosition(spAnimal.getSelectedItemPosition()).toString();
+                Toast.makeText(getApplicationContext(),animal,Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // No hacer nada
+            }
+
+        });
+
+    }
 
         /*spAnimal.setOnItemSelectedListener(this);
 
@@ -50,7 +80,7 @@ public class ConsultaAnimalesActivity extends AppCompatActivity {
             public void onClick(View v) {
                 buscarAnimal("http://protectora-animales.ddns.net/phpMyAdmin/api/animals/getAnimals.php");
             }
-        });*/
+        });
     }
 
         private void buscarAnimal(String URL){
@@ -82,9 +112,51 @@ public class ConsultaAnimalesActivity extends AppCompatActivity {
             );
             requestQueue= Volley.newRequestQueue(this);
             requestQueue.add(jsonArrayRequest);
-        }
+        }*/
 
 
+
+
+
+    private void buscarAnimal(String url) {
+        RequestQueue requestQueue=Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject=new JSONObject(response);
+                    if(jsonObject.getInt("success")==1){
+                        JSONArray jsonArray=jsonObject.getJSONArray("name");
+                        for(int i=0;i<jsonArray.length();i++){
+                            JSONObject jsonObject1=jsonArray.getJSONObject(i);
+                            String animal=jsonObject1.getString("id");
+                            txtNacimiento.setText(jsonObject.getString("fechaNacimientoAnimales"));
+                            txtTipo.setText(jsonObject.getString("tipoAnimales"));
+                            txtEstado.setText(jsonObject.getString("descripcionEstadoAnimales"));
+                            /*String animal=jsonObject1.getString("birthDate");
+                            String animal=jsonObject1.getString("type");
+                            String animal=jsonObject1.getString("state");*/
+                            lstAnimales.add(animal);
+                        }
+                    }
+
+                    spAnimal.setAdapter(new ArrayAdapter<String>(ConsultaAnimalesActivity.this, android.R.layout.simple_spinner_dropdown_item, lstAnimales));
+                }catch (JSONException e){e.printStackTrace();}
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+        });
+
+        int socketTimeout = 30000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        requestQueue.add(stringRequest);
 
     }
+}
 
--- app/src/main/java/com/example/protectora/MainActivity.java
+++ app/src/main/java/com/example/protectora/MainActivity.java
@@ -4,61 +4,84 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
-    EditText txtNombreUsuario, edittxtContrasenaUsuario;
-    Button entrar;
+    EditText txtNombreUsuario, txtContrasenaUsuario;
+    Button btnEntrar;
+    JSONArray ja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtNombreUsuario = (EditText) findViewById(R.id.txtNombreUsuario);
+        txtContrasenaUsuario = (EditText) findViewById(R.id.txtContrasenaUsuario);
 
-        entrar = findViewById(R.id.btnEntrar);
-        entrar.setOnClickListener(new View.OnClickListener() {
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent entrar = new Intent(MainActivity.this, com.example.protectora.Principal.class);
-                startActivity(entrar);
+                entrarPass("http://protectora-animales.ddns.net/phpMyAdmin/api/login/consultarusuario.php?user=" + txtNombreUsuario.getText().toString());
             }
         });
+    }
+    private void entrarPass(String URL) {
 
-/*
+        Log.i("url",""+URL);
 
-        entrar = (Button)findViewById(R.id.btnEntrar);
-        txtNombreUsuario = (EditText) findViewById(R.id.txtNombreUsuario);
-        edittxtContrasenaUsuario = (EditText) findViewById(R.id.txtContrasenaUsuario);
-
-        entrar.setOnClickListener(new View.OnClickListener() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest =  new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
             @Override
-            public void onClick(View view) {
-                usuarioLogin();
+            public void onResponse(String response) {
+
+                try {
+                    ja = new JSONArray(response);
+                    String contra = ja.getString(0);
+                    if(contra.equals(txtContrasenaUsuario.getText().toString())){
+
+                        Toast.makeText(getApplicationContext(),"Conexión correcta",Toast.LENGTH_SHORT).show();
+                        Intent entrar = new Intent(MainActivity.this, com.example.protectora.Principal.class);
+                        startActivity(entrar);
+
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Contraseña incorrecta",Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(getApplicationContext(),"Nombre de usuario incorrecto",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
             }
         });
+
+        queue.add(stringRequest);
+
+
     }
 
-    private void usuarioLogin() {
-        final String usuarioNombre = txtNombreUsuario.getText().toString();
-        final String usuarioContrasena = edittxtContrasenaUsuario.getText().toString();
 
-        if (TextUtils.isEmpty(usuarioNombre)) {
-            txtNombreUsuario.setError("Por favor, introduce tu nombre de usuario");
-            txtNombreUsuario.requestFocus();
-            return;
-        }
-
-        if (TextUtils.isEmpty(usuarioContrasena)) {
-            edittxtContrasenaUsuario.setError("Por favor, introduce una contraseña");
-            edittxtContrasenaUsuario.requestFocus();
-            return;
-
-
-            //PRUEBA DE AQUÍ AL FINAL
-            
-
-        }*/
-    }
 }
